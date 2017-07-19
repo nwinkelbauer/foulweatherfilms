ca49097273ec6331c78c059cd7323394f73f093d
Nick Winkelbauer <nwinkelb@uw.edu>
2017-07-18 21:08:22 -0700

    build

--- src/scripts/flux/actions/DataActions.js
+++ src/scripts/flux/actions/DataActions.js
@@ -4,8 +4,8 @@ import alt   from 'flux/alt/alt.js';
 class DataActions {
 
     constructor() {
-        const appUrl = 'http://fwf.dev/index.php'; // Work Wordpress installation url
-        //const appUrl = 'http://foulweatherfilms.com/cms'; //home laptop install
+        //const appUrl = 'http://fwf.dev/index.php'; // Work Wordpress installation url
+        const appUrl = 'http://foulweatherfilms.com/cms'; //live
 
         this.pagesEndPoint = `${appUrl}/wp-json/wp/v2/pages?per_page=99`; // Endpoint for getting Wordpress Pages
         this.postsEndPoint = `${appUrl}/wp-json/wp/v2/posts?per_page=15`; // Endpoint for getting Wordpress Posts
